a8f53db39eb954828c62052b3922de2e1da21f87
sandeep sharma <sandeepsharma8922@gmail.com>
2021-05-26 15:53:36 +0530

    Updates

---
+++ precache-manifest.87e86246016527d5173444fd59e688bd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38e154a600b80421122225a2e7653ca",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "7af39346690bda6fdb1d",
+    "url": "/reactjs/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1df6c9422f3677bbfab2",
+    "url": "/reactjs/static/css/main.d7f46a7a.chunk.css"
+  },
+  {
+    "revision": "7af39346690bda6fdb1d",
+    "url": "/reactjs/static/js/2.fdbce57f.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactjs/static/js/2.fdbce57f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df6c9422f3677bbfab2",
+    "url": "/reactjs/static/js/main.d7b69071.chunk.js"
+  },
+  {
+    "revision": "96729a946eaa9dc508db",
+    "url": "/reactjs/static/js/runtime-main.2293d5b2.js"
+  },
+  {
+    "revision": "82d18650bbe7a17f883ea60b6b57fcb3",
+    "url": "/reactjs/static/media/3d1.82d18650.jpg"
+  },
+  {
+    "revision": "46ba12b38aecf58868ebb41d414c4bcd",
+    "url": "/reactjs/static/media/kk6.46ba12b3.png"
+  },
+  {
+    "revision": "5720c65c936310760475f9d48e519915",
+    "url": "/reactjs/static/media/micro1.5720c65c.jpg"
+  },
+  {
+    "revision": "9782610bee5503be9218b2788985f680",
+    "url": "/reactjs/static/media/micro4.9782610b.jpg"
+  },
+  {
+    "revision": "76e6b707765a3d8b572e3bb1da8bdd2b",
+    "url": "/reactjs/static/media/today2.76e6b707.jpg"
+  },
+  {
+    "revision": "5d158313a844943f4a215c5efbdc5947",
+    "url": "/reactjs/static/media/web3.5d158313.jpeg"
+  },
+  {
+    "revision": "4ff9ab09d261cb69c2273bd150b57be0",
+    "url": "/reactjs/static/media/web8.4ff9ab09.jpeg"
+  }
+]);
